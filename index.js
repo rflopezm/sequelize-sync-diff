eff5724ffe9c47ff1a10e331d309d1fe289c3519
Rodrigo Lopez <rflopezm@gmail.com>
2024-03-25 23:46:39 -0400

    upgrade library

--- index.js
+++ index.js
@@ -1,45 +1,55 @@
-var dbdiff = require('dbdiff')
+var dbdiff = require("dbdiff");
 
-module.exports = function(Sequelize, diffOptions) {
-  diffOptions = diffOptions || {level: 'drop'};
-  if (Sequelize.prototype.syncDiff) return
+module.exports = function (Sequelize, diffOptions) {
+  diffOptions = diffOptions || { level: "drop" };
+  if (Sequelize.prototype.syncDiff) return;
 
-  var sequelizeMethods = ['define']
-  var modelMethods = ['belongsTo', 'hasOne', 'hasMany', 'belongsToMany']
+  var sequelizeMethods = ["define"];
+  var modelMethods = ["belongsTo", "hasOne", "hasMany", "belongsToMany"];
 
-  sequelizeMethods.forEach(function(name) {
-    var func = Sequelize.prototype[name]
-    Sequelize.prototype[name] = function() {
-      var args = Array.prototype.slice.call(arguments)
-      this.diff_actions = this.diff_actions || []
-      this.diff_actions.push({ method: name, args: args })
-      return func.apply(this, args)
-    }
-  })
+  sequelizeMethods.forEach(function (name) {
+    var func = Sequelize.prototype[name];
+    Sequelize.prototype[name] = function () {
+      var args = Array.prototype.slice.call(arguments);
+      this.diff_actions = this.diff_actions || [];
+      this.diff_actions.push({ method: name, args: args });
+      return func.apply(this, args);
+    };
+  });
 
-  modelMethods.forEach(function(name) {
-    var func = Sequelize.Model.prototype[name]
-    Sequelize.Model.prototype[name] = function() {
-      var args = Array.prototype.slice.call(arguments)
-      var sequelize = this.sequelize
-      sequelize.diff_actions = sequelize.diff_actions || []
-      sequelize.diff_actions.push({ method: name, args: args, model: this.name })
-      return func.apply(this, args)
-    }
-  })
+  modelMethods.forEach(function (name) {
+    var func = Sequelize.Model.prototype[name];
+    Sequelize.Model.prototype[name] = function () {
+      var args = Array.prototype.slice.call(arguments);
+      var sequelize = this.sequelize;
+      sequelize.diff_actions = sequelize.diff_actions || [];
+      sequelize.diff_actions.push({
+        method: name,
+        args: args,
+        model: this.name,
+      });
+      return func.apply(this, args);
+    };
+  });
 
-  Sequelize.prototype.syncDiff = function(url, options) {
-    var self = this
-    var sequelize = new Sequelize(url, options || this.options)
-    this.diff_actions.forEach(function(action) {
+  Sequelize.prototype.syncDiff = function (options) {
+    var self = this;
+    const allOptions = { ...this.options, database: options.database };
+    var sequelize = new Sequelize(
+      options.database,
+      options.username,
+      options.password,
+      allOptions
+    );
+    this.diff_actions.forEach(function (action) {
       if (!action.model) {
-        sequelize[action.method].apply(sequelize, action.args)
+        sequelize[action.method].apply(sequelize, action.args);
       } else {
-        var model = sequelize.model(action.model)
-        action.args[0] = sequelize.model(action.args[0].name)
-        model[action.method].apply(model, action.args)
+        var model = sequelize.model(action.model);
+        action.args[0] = sequelize.model(action.args[0].name);
+        model[action.method].apply(model, action.args);
       }
-    })
+    });
 
     var selfOptions = {
       dialect: self.options.dialect,
@@ -48,15 +58,19 @@ module.exports = function(Sequelize, diffOptions) {
       database: self.config.database,
       port: self.config.port,
       host: self.config.host,
-      dialectOptions: self.config.dialectOptions
-    }
-    var diff = new dbdiff.DbDiff()
-    return sequelize.sync({ force: true })
-      .then(function() {
-        return diff.compare(selfOptions, url);
+      dialectOptions: self.config.dialectOptions,
+    };
+    var diff = new dbdiff.DbDiff();
+    return sequelize
+      .sync({ force: true })
+      .then(function () {
+        return diff.compare(selfOptions, {
+          ...selfOptions,
+          database: options.database,
+        });
       })
-      .then(function() {
+      .then(function () {
         return diff.commands(diffOptions.level);
       });
-  }
-}
+  };
+};
